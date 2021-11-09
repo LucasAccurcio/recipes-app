0ff8124ee8e1e018eec4ff4bf34db695e9ad3332
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-11-09 17:48:11 -0300

    Adiciona botoes nas rotas /explorar/comidas e /explorar/bebidas

--- src/pages/Filtrar.js
+++ src/pages/Filtrar.js
@@ -6,6 +6,7 @@ import Header from '../components/Header';
 function Filtrar() {
   const history = useHistory();
   const { location: { pathname } } = history;
+  const isPageDrinks = pathname === '/explorar/bebidas';
   console.log(pathname);
   let atualComponent = 'Explorar';
   if (pathname === ('/explorar/comidas')) {
@@ -14,10 +15,29 @@ function Filtrar() {
     atualComponent = 'Explorar Bebidas';
   }
 
+  function showButtonOrigin() {
+    return (
+      <button data-testid="explore-by-area" type="button">
+        Por Local de Origem
+      </button>
+    );
+  }
+
   return (
     <section>
       <Header componentName={ atualComponent } />
       <h1>Filtrar</h1>
+      <section>
+        <button data-testid="explore-by-ingredient" type="button">
+          Por Ingredientes
+        </button>
+
+        { !isPageDrinks && showButtonOrigin() }
+
+        <button data-testid="explore-surprise" type="button">
+          Me Surpreenda!
+        </button>
+      </section>
       <Footer />
     </section>
   );
