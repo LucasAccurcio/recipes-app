2c9a686173ec59cd22ed538b0c663e5fb2e75ed3
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-11-09 15:49:55 -0300

    Tela Explorar

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -7,6 +8,19 @@ function Explorar() {
     <section>
       <Header />
       <h1>Explorar</h1>
+      <section>
+        <Link to="/explorar/comidas">
+          <button data-testid="explore-food" type="button">
+            Explorar Comidas
+          </button>
+        </Link>
+
+        <Link to="/explorar/bebidas">
+          <button data-testid="explore-drinks" type="button">
+            Explorar Bebidas
+          </button>
+        </Link>
+      </section>
       <Footer />
     </section>
   );
