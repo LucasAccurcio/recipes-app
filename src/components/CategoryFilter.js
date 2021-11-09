4785f0f1ad110b7e94c6adb8c402de2ebc3b23e4
LucasAccurcio <lucas.accurcio@gmail.com>
2021-11-09 18:54:52 -0300

    Implementaçao componente Filtrar por Categoria

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import fetchAPI from '../services/fetchAPI';
+
+const INITIAL_STATE = [{ strCategory: 'All' }];
+
+export default function CategoryFilter({ url }) {
+  const [category, setCategory] = useState(INITIAL_STATE);
+  console.log(category);
+  const getCategory = async () => {
+    const categoryFilter = await fetchAPI(url);
+    const MAX_CATEGORY = 5;
+    const page = url.split('www.')[1].split('.com')[0];
+    if (page === 'themealdb') {
+      setCategory([...category, ...categoryFilter.meals
+        .filter((_, index) => index < MAX_CATEGORY)]);
+      return null;
+    }
+    setCategory([...category, ...categoryFilter.drinks
+      .filter((_, index) => index < MAX_CATEGORY)]);
+  };
+
+  useEffect(() => {
+    getCategory();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <nav>
+      { category && category
+        .map((cat, index) => (
+          <button
+            key={ index }
+            type="button"
+            data-testid={ `${cat.strCategory}-category-filter` }
+          >
+            { cat.strCategory }
+          </button>
+        )) }
+    </nav>
+  );
+}
+
+CategoryFilter.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
+// meal 'https://www.themealdb.com/api/json/v1/1/list.php?c=list'
+// drink 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
