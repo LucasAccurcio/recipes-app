82e9f5293cb5d29d71c22f2d31877fdb58a628de
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-11-11 10:50:46 -0300

    Adciona botão na página notFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
     <section>
       <h1>Not Found</h1>
+      <Link to="/comidas">
+        <button className="btn btn-secondary" type="button">
+          Home
+        </button>
+      </Link>
     </section>
   );
 }
