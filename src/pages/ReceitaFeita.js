4b4a3c749648cded3d0a41adbfad620553788d6f
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-11-17 10:50:14 -0300

    Corrigi erro nos testes de requisitos já aprovados

--- src/pages/ReceitaFeita.js
+++ src/pages/ReceitaFeita.js
@@ -6,24 +6,25 @@ function ReceitaFeita() {
   return (
     <section>
       <Header componentName="Receitas Feitas" />
-      <section>
+      <h1>Receita Feita</h1>
+      {/* <section>
         <button data-testid="filter-by-all-btn" type="button">All</button>
         <button data-testid="filter-by-food-btn" type="button">Food</button>
         <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
       </section>
       <section>
-        {/* { receitas.meals.map(({ strMeal, strMealThumb, strCategory }, index) => (
-        <CardReceitaFeita
-          key={ index }
-          name={ strMeal }
-          img={ strMealThumb }
-          category={ strCategory }
-          index={ index }
-          data="16/11/2021"
-          tags=" "
-        />
-      )) } */}
-      </section>
+        { receitas.meals.map(({ strMeal, strMealThumb, strCategory }, index) => (
+          <CardReceitaFeita
+            key={ index }
+            name={ strMeal }
+            img={ strMealThumb }
+            category={ strCategory }
+            index={ index }
+            data="16/11/2021"
+            tags=" "
+          />
+        )) }
+      </section> */}
     </section>
   );
 }
--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import Header from '../components/Header';
-import CardReceitaFavorita from '../components/CardReceitaFavorita';
+// import CardReceitaFavorita from '../components/CardReceitaFavorita';
 
 function ReceitasFavoritas() {
-  function getFavoriteStorage() {
-    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    if (favoriteRecipes.length === 0) {
-      return global.alert('Não existem receitas favoritas');
-    }
-    return favoriteRecipes;
-  }
+  // function getFavoriteStorage() {
+  //   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  //   if (favoriteRecipes.length === 0) {
+  //     return global.alert('Não existem receitas favoritas');
+  //   }
+  //   return favoriteRecipes;
+  // }
 
   return (
     <section>
       <Header componentName="Receitas Favoritas" />
-      <section>
+      {/* <section>
         <button data-testid="filter-by-all-btn" type="button">All</button>
         <button data-testid="filter-by-food-btn" type="button">Food</button>
         <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
       </section>
-      { getFavoriteStorage().map(({ name, image, category }, index) => (
+      { getFavoriteStorage().map(({ name, image, category, alcoholicOrNot }, index) => (
         <CardReceitaFavorita
           key={ index }
           name={ name }
           img={ image }
+          alcoholic={ alcoholicOrNot }
           category={ category }
         />
-      )) }
+      )) } */}
     </section>
   );
 }
