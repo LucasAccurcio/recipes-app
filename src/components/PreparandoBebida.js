cc4a051d970b9cfca249b37a2e27bdd74b799e5b
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-11-17 19:33:58 -0300

    Requisito 47 - Concluido

--- src/components/PreparandoBebida.js
+++ src/components/PreparandoBebida.js
@@ -1,109 +1,118 @@
-// import React, { useContext } from 'react';
-// import Modal from './Modal';
-// import Context from '../context/Context';
+import React, { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import Modal from './Modal';
+import Context from '../context/Context';
+import shareIcon from '../images/shareIcon.svg';
+import FavoriteDrinksButton from './FavoriteDrinksButton';
+import fetchAPI from '../services/fetchAPI';
 
-// function PreparandoBebida() {
-//   const { drinks } = useContext(Context);
-//   const [modal, setModal] = useState(false);
-//   const THREE_SECONDS = 3000;
+function PreparandoBebida() {
+  const { drinks, setDrinks } = useContext(Context);
+  const [loading, setLoading] = useState(false);
+  const [modal, setModal] = useState(false);
+  const THREE_SECONDS = 3000;
 
-// function getIngredientes(list) {
-//   const ingredientes = [];
-//   const NUMBER_OF_INGREDIENTS = 15;
-//   for (let index = 1; index <= NUMBER_OF_INGREDIENTS; index += 1) {
-//     const ingred = list[`strIngredient${index}`];
-//     const quantidade = list[`strMeasure${index}`];
-//     if (ingred !== '' && quantidade !== '') {
-//       ingredientes.push(`${quantidade} - ${ingred}`);
-//     }
-//   }
-//   return ingredientes;
-// }
+  const { location: { pathname } } = useHistory();
+  const idPage = pathname.split('/')[2];
+  const URL_DRINKS = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idPage}`;
 
-//   return (
-//     <section>
-//       <div>
-//         <Modal modal={ modal } />
-//         <img
-//           data-testid="recipe-photo"
-//           className="image-recipe"
-//           src={ drinks.strDrinkThumb }
-//           alt="receita"
-//         />
-//         <div className="title-container">
-//           <div className="text-container">
-//             <h2 className="margin-txt" data-testid="recipe-title">{drinks.strDrink}</h2>
-//             <h4
-//               className="category margin-txt"
-//               data-testid="recipe-category"
-//             >
-//               {drinks.strCategory}
-//               {` (${drinks.strAlcoholic})`}
-//             </h4>
-//           </div>
-//           <button
-//             type="button"
-//             onClick={ () => {
-//               window.navigator.clipboard.writeText(window.location.href);
-//               setModal(true);
-//               setTimeout(() => {
-//                 setModal(false);
-//               }, THREE_SECONDS);
-//             } }
-//           >
-//             <img
-//               data-testid="share-btn"
-//               className="icon"
-//               src={ shareIcon }
-//               alt="share icon"
-//             />
-//           </button>
-//           <button
-//             type="button"
-//             onClick={ favoritar }
-//           >
-//             <img
-//               data-testid="favorite-btn"
-//               className="icon"
-//               src={ favorito.img }
-//               alt="favorite"
-//             />
-//           </button>
-//         </div>
-//         <div className="ingredient-container">
-//           <h5>Ingredientes</h5>
-//           <div className="ingredients">
-//             { getIngredients(drinks).map((item, index) => (
-//               item !== 'null - null'
-//             && (
-//               <label htmlFor={ item }>
-//                 <input
-//                   type="checkbox"
-//                   key={ index }
-//                   data-testid={ `${index}-ingredient-name-and-measure` }
-//                   name={ item }
-//                 />
-//                 { item }
-//               </label>
-//             )
-//             )) }
-//           </div>
-//         </div>
-//         <div className="instructions">
-//           <p data-testid="instructions">{drinks.strInstructions}</p>
-//         </div>
-//         <div className="start-btn-container">
-//           <button
-//             className="start-btn"
-//             type="button"
-//             data-testid="finish-recipe-btn"
-//           >
-//             Finalizar Receita
-//           </button>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
+  function fetchDrinks(endpoint) {
+    fetchAPI(endpoint)
+      .then((response) => setDrinks(response.drinks[0]));
+    setLoading(true);
+  }
 
-// export default PreparandoBebida;
+  useEffect(() => {
+    fetchDrinks(URL_DRINKS);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function getIngredientes(list) {
+    const ingredientes = [];
+    const NUMBER_OF_INGREDIENTS = 15;
+    for (let index = 1; index <= NUMBER_OF_INGREDIENTS; index += 1) {
+      const ingred = list[`strIngredient${index}`];
+      const quantidade = list[`strMeasure${index}`];
+      if (ingred !== '' && quantidade !== '') {
+        ingredientes.push(`${quantidade} - ${ingred}`);
+      }
+    }
+    return ingredientes;
+  }
+
+  return (
+    <section>
+      <div>
+        <Modal modal={ modal } />
+        <img
+          data-testid="recipe-photo"
+          className="image-recipe"
+          src={ drinks.strDrinkThumb }
+          alt="receita"
+        />
+        <div className="title-container">
+          <div className="text-container">
+            <h2 className="margin-txt" data-testid="recipe-title">{drinks.strDrink}</h2>
+            <h4
+              className="category margin-txt"
+              data-testid="recipe-category"
+            >
+              {drinks.strAlcoholic}
+            </h4>
+          </div>
+          <button
+            type="button"
+            onClick={ () => {
+              window.navigator.clipboard.writeText(window.location.href);
+              setModal(true);
+              setTimeout(() => {
+                setModal(false);
+              }, THREE_SECONDS);
+            } }
+          >
+            <img
+              data-testid="share-btn"
+              className="icon"
+              src={ shareIcon }
+              alt="share icon"
+            />
+          </button>
+          <FavoriteDrinksButton />
+        </div>
+        <div className="ingredient-container">
+          <h5>Ingredientes</h5>
+          <div className="ingredients">
+            { loading && getIngredientes(drinks).map((item, index) => (
+              item !== 'null - null'
+            && (
+              <label htmlFor={ item } key={ index }>
+                <input
+                  type="checkbox"
+                  id={ item }
+                  data-testid={ `${index}-ingredient-step` }
+                  name={ item }
+                />
+                { item }
+              </label>
+            )
+            )) }
+          </div>
+        </div>
+        <div className="instructions">
+          <p data-testid="instructions">{drinks.strInstructions}</p>
+        </div>
+        <div className="start-btn-container">
+          <button
+            className="start-btn"
+            type="button"
+            data-testid="finish-recipe-btn"
+          >
+            Finalizar Receita
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PreparandoBebida;
--- src/components/PreparandoComida.js
+++ src/components/PreparandoComida.js
@@ -1,108 +1,118 @@
-// import React, { useContext } from 'react';
-// import Modal from './Modal';
-// import Context from '../context/Context';
+import React, { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import Modal from './Modal';
+import Context from '../context/Context';
+import shareIcon from '../images/shareIcon.svg';
+import Button from './Button';
+import fetchAPI from '../services/fetchAPI';
 
-// function PreparandoComida() {
-//   const { comida } = useContext(Context);
-//   const [modal, setModal] = useState(false);
-//   const THREE_SECONDS = 3000;
+function PreparandoComida() {
+  const { comida, setComida } = useContext(Context);
+  const [loading, setLoading] = useState(false);
+  const [modal, setModal] = useState(false);
+  const THREE_SECONDS = 3000;
 
-// function getIngredientes(list) {
-//   const ingredientes = [];
-//   const NUMBER_OF_INGREDIENTS = 15;
-//   for (let index = 1; index <= NUMBER_OF_INGREDIENTS; index += 1) {
-//     const ingred = list[`strIngredient${index}`];
-//     const quantidade = list[`strMeasure${index}`];
-//     if (ingred !== '' && quantidade !== '') {
-//       ingredientes.push(`${quantidade} - ${ingred}`);
-//     }
-//   }
-//   return ingredientes;
-// }
+  const { location: { pathname } } = useHistory();
+  const idPage = pathname.split('/')[2];
+  const URL_MEALS = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idPage}`;
 
-//   return (
-//     <section>
-//       <div>
-//         <Modal modal={ modal } />
-//         <img
-//           data-testid="recipe-photo"
-//           className="image-recipe"
-//           src={ comida.strMealThumb }
-//           alt="receita"
-//         />
-//         <div className="title-container">
-//           <div className="text-container">
-//             <h2 className="margin-txt" data-testid="recipe-title">{comida.strMeal}</h2>
-//             <h4
-//               className="category margin-txt"
-//               data-testid="recipe-category"
-//             >
-//               {comida.strCategory}
-//             </h4>
-//           </div>
-//           <button
-//             type="button"
-//             onClick={ () => {
-//               window.navigator.clipboard.writeText(window.location.href);
-//               setModal(true);
-//               setTimeout(() => {
-//                 setModal(false);
-//               }, THREE_SECONDS);
-//             } }
-//           >
-//             <img
-//               data-testid="share-btn"
-//               className="icon"
-//               src={ shareIcon }
-//               alt="share icon"
-//             />
-//           </button>
-//           <button
-//             type="button"
-//             onClick={ favoritar }
-//           >
-//             <img
-//               data-testid="favorite-btn"
-//               className="icon"
-//               src={ favorito.img }
-//               alt="favorite"
-//             />
-//           </button>
-//         </div>
-//         <div className="ingredient-container">
-//           <h5>Ingredientes</h5>
-//           <div className="ingredients">
-//             { getIngredientes(comida).map((item, index) => (
-//               item !== 'null - null'
-//             && (
-//               <label htmlFor={ item }>
-//                 <input
-//                   type="checkbox"
-//                   key={ index }
-//                   data-testid={ `${index}-ingredient-name-and-measure` }
-//                   name={ item }
-//                 />
-//                 { item }
-//               </label>
-//             )
-//             )) }
-//           </div>
-//         </div>
-//         <div className="instructions">
-//           <p data-testid="instructions">{comida.strInstructions}</p>
-//         </div>
-//         <div className="start-btn-container">
-//           <button
-//             className="start-btn"
-//             type="button"
-//             data-testid="finish-recipe-btn"
-//           >
-//             Finalizar Receita
-//           </button>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
+  function fetchComida(data) {
+    fetchAPI(data)
+      .then((response) => setComida(response.meals[0]));
+    setLoading(true);
+  }
 
-// export default PreparandoComida;
+  useEffect(() => {
+    fetchComida(URL_MEALS);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function getIngredientes() {
+    const ingredientes = [];
+    const NUMBER_OF_INGREDIENTS = 15;
+    for (let index = 1; index <= NUMBER_OF_INGREDIENTS; index += 1) {
+      const ingred = comida[`strIngredient${index}`];
+      const quantidade = comida[`strMeasure${index}`];
+      if (ingred !== '' && quantidade !== '') {
+        ingredientes.push(`${quantidade} - ${ingred}`);
+      }
+    }
+    return ingredientes;
+  }
+
+  return (
+    <section>
+      <div>
+        <Modal modal={ modal } />
+        <img
+          data-testid="recipe-photo"
+          className="image-recipe"
+          src={ comida.strMealThumb }
+          alt="receita"
+        />
+        <div className="title-container">
+          <div className="text-container">
+            <h2 className="margin-txt" data-testid="recipe-title">{comida.strMeal}</h2>
+            <h4
+              className="category margin-txt"
+              data-testid="recipe-category"
+            >
+              {comida.strCategory}
+            </h4>
+          </div>
+          <button
+            type="button"
+            onClick={ () => {
+              window.navigator.clipboard.writeText(window.location.href);
+              setModal(true);
+              setTimeout(() => {
+                setModal(false);
+              }, THREE_SECONDS);
+            } }
+          >
+            <img
+              data-testid="share-btn"
+              className="icon"
+              src={ shareIcon }
+              alt="share icon"
+            />
+          </button>
+          <Button />
+        </div>
+        <div className="ingredient-container">
+          <h5>Ingredientes</h5>
+          <div className="ingredients">
+            { loading && getIngredientes().map((item, index) => (
+              item !== 'null - null'
+            && (
+              <label htmlFor={ item } key={ index }>
+                <input
+                  type="checkbox"
+                  id={ item }
+                  data-testid={ `${index}-ingredient-step` }
+                  name="checkbox"
+                />
+                { item }
+              </label>
+            )
+            )) }
+          </div>
+        </div>
+        <div className="instructions">
+          <p data-testid="instructions">{comida.strInstructions}</p>
+        </div>
+        <div className="start-btn-container">
+          <button
+            className="start-btn"
+            type="button"
+            data-testid="finish-recipe-btn"
+          >
+            Finalizar Receita
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PreparandoComida;
