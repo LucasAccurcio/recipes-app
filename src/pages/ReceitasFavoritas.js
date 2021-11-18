254d1f54ea2368d5fd33a0adedaefb05679ae2cc
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-11-18 01:03:17 -0300

    Requisito 65

--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import CardComidaFavorita from '../components/CardComidaFavorita';
 import CardBebidaFavorita from '../components/CardBebidaFavorita';
 
 function ReceitasFavoritas() {
+  const [filter, setFilter] = useState({
+    recipe: [],
+    isFilter: false,
+  });
   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+  function setFilterMeal() {
+    const filterComidas = favoriteRecipes.filter(({ type }) => type === 'comida');
+    setFilter({
+      recipe: filterComidas,
+      isFilter: true,
+    });
+  }
+
+  function setFilterDrink() {
+    const filterComidas = favoriteRecipes.filter(({ type }) => type === 'bebida');
+    setFilter({
+      recipe: filterComidas,
+      isFilter: true,
+    });
+  }
+
+  function setFilterAll() {
+    setFilter({
+      recipe: [],
+      isFilter: false,
+    });
+  }
 
   function showRecipeFavorite() {
     return (
       <section>
-        {favoriteRecipes && favoriteRecipes.map((recipe, index) => (
+        {!filter.isFilter && favoriteRecipes.map((recipe, index) => (
           (recipe.type === 'comida'
             ? <CardComidaFavorita key={ index } meal={ recipe } index={ index } />
             : <CardBebidaFavorita key={ index } drink={ recipe } index={ index } />)
+        )) }
+      </section>
+    );
+  }
+
+  function showRecipeFilter() {
+    const { recipe } = filter;
+    return (
+      <section>
+        {recipe.map((meal, index) => (
+          (recipe.type === 'comida'
+            ? <CardComidaFavorita key={ index } meal={ meal } index={ index } />
+            : <CardBebidaFavorita key={ index } drink={ meal } index={ index } />)
         )) }
       </section>
     );
@@ -22,10 +62,29 @@ function ReceitasFavoritas() {
     <section>
       <Header componentName="Receitas Favoritas" />
       <section>
-        <button data-testid="filter-by-all-btn" type="button">All</button>
-        <button data-testid="filter-by-food-btn" type="button">Food</button>
-        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          onClick={ setFilterAll }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          onClick={ setFilterMeal }
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          onClick={ setFilterDrink }
+        >
+          Drinks
+        </button>
       </section>
+      { filter.isFilter && showRecipeFilter() }
       { showRecipeFavorite() }
     </section>
   );
